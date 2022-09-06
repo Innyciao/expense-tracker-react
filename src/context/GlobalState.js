965b882bbb9f3d434b271351a9fb3f261d29cadb
innyciao <abosedeolaoyec@gmail.com>
2022-09-06 08:53:46 +0100

    React-context expense tracker

---
+++ src/context/GlobalState.js
@@ -0,0 +1,41 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+// initial state
+const intialState = {
+    transactions: []
+}
+
+// create context
+export const GlobalContext = createContext(intialState);
+
+//  provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, intialState);
+
+    // Actions
+
+    function deleteTransaction (id){
+        dispatch({
+            type : 'DELETE_TRANSACTION',
+            payload : id
+        })
+    }
+
+    function addTransaction (transaction){
+        dispatch({
+            type : 'ADD_TRANSACTION',
+            payload : transaction
+        })
+    }
+
+    return (<GlobalContext.Provider
+        value={{
+            transactions: state.transactions,
+            deleteTransaction,
+            addTransaction
+        }}>
+        {children}
+    </GlobalContext.Provider>)
+}
+
